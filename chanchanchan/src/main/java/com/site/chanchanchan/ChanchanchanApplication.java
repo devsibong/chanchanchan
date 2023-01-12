0511dfe8bd91af05ab164379dda7aae6f4796a4a
devsibong <seohae7785@gmail.com>
2023-01-12 12:30:56 +0900

    build: chanchanchan 프로젝트 생성

---
+++ chanchanchan/src/main/java/com/site/chanchanchan/ChanchanchanApplication.java
@@ -0,0 +1,13 @@
+package com.site.chanchanchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanchanchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanchanchanApplication.class, args);
+	}
+
+}
