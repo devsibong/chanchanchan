77a1f572a14dc57199a07e97513acc4e0fd26635
devsibong <seohae7785@gmail.com>
2023-01-13 13:11:23 +0900

    modify: Maven 수정

---
+++ chanchanchan/src/main/java/com/site/chanchanchan/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.site.chanchanchan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String main() {
+		return "layout";
+	}
+}
