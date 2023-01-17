3e4edacbb26e297f42bfa7246666298e8b06e169
JeongSeulGi <zhem925@gmail.com>
2023-01-17 18:10:53 +0900

    pull

--- chanchanchan/src/main/java/com/site/chanchanchan/controller/MemberController.java
+++ chanchanchan/src/main/java/com/site/chanchanchan/controller/MemberController.java
@@ -12,18 +12,36 @@ import com.site.chanchanchan.service.MemberService;
 
 
 @Controller
-@RequestMapping("/member")
 public class MemberController {
 
-	String dir = "member/";
+	String dir = "login/";
 
 	@Autowired
-	MemberService mem_service;
+	MemberService memservice;
 	
 	@RequestMapping("/login")
 	public String login(Model model) {
 		model.addAttribute("center", dir + "login");
 		return "main";
+	}
+	
+	@RequestMapping("/loginimpl")
+	public String loginimpl(HttpSession session, String id, String pwd, Model model) {
+		Member mem = null;
+		String result = "loginfail";
+		try {
+			mem = memservice.get(id);
+			if(mem != null) {
+				if(mem.getMember_pw().equals(pwd)) { // 성공
+					result = "login";
+					session.setAttribute("loginmem", mem);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("center", result);
+		return "redirect:/";
 	}
 
 	@RequestMapping("/logout")
