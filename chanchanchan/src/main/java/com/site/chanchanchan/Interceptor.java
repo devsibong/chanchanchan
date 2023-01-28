87bc84a5869f332f47cd9e0bbc0dfcb5ddf79902
devsibong <seohae7785@gmail.com>
2023-01-28 13:42:37 +0900

    add: Interceptor 추가

---
+++ chanchanchan/src/main/java/com/site/chanchanchan/Interceptor.java
@@ -0,0 +1,39 @@
+package com.site.chanchanchan;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.site.chanchanchan.dto.Category;
+import com.site.chanchanchan.service.CategoryService;
+
+public class Interceptor implements HandlerInterceptor {
+	
+	@Autowired
+	CategoryService categoryservice;
+	 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return true;
+    }
+ 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+    	List<Category> catelist = categoryservice.get();
+    	modelAndView.addObject("catelist", catelist);
+        
+    }
+ 
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+    }    
+    
+}
