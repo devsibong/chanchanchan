699c0d2c533a362d9ff32abc9088fd637b8e0848
Kyoung Tae <113958094+Park-KyoungTae@users.noreply.github.com>
2023-02-01 18:02:46 +0900

    02/01 박경태 관리자페이지 인터셉터 완료

---
+++ chanchanchanadmin/src/main/java/com/site/chanchanchan/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.site.chanchanchan;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    	registry.addInterceptor(new Interceptor())
+    			.addPathPatterns("/**")
+    			.excludePathPatterns("/login")
+    			.excludePathPatterns("/**/*.js")
+    			.excludePathPatterns("/**/*.css")
+    			.excludePathPatterns("/**/**/*.css")
+    			.excludePathPatterns("/**/**/*.js")
+    			.excludePathPatterns("/**/**/**/*.css")
+    			.excludePathPatterns("/**/**/**/*.js")
+    			.excludePathPatterns("/login");
+    }
+}
