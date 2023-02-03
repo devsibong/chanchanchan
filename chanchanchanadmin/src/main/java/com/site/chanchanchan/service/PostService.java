3b1fae6358d6d95f28c743e09661f9dbbe4f507c
Kyoung Tae <113958094+Park-KyoungTae@users.noreply.github.com>
2023-02-04 02:32:07 +0900

    02/03 박경태 관리자페이지 1대1 문의 목록 띄우기

---
+++ chanchanchanadmin/src/main/java/com/site/chanchanchan/service/PostService.java
@@ -0,0 +1,54 @@
+package com.site.chanchanchan.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.site.chanchanchan.dto.Criteria;
+import com.site.chanchanchan.dto.Post;
+import com.site.chanchanchan.frame.MyService;
+import com.site.chanchanchan.mapper.PostMapper;
+
+@Service
+public class PostService implements MyService<Integer, Post>{
+
+	@Autowired
+	PostMapper postmapper;
+	
+	@Override
+	public void register(Post v) throws Exception {
+		postmapper.insert(v);
+	}
+
+	@Override
+	public void modify(Post v) throws Exception {
+		postmapper.update(v);
+	}
+
+	@Override
+	public void remove(Integer k) throws Exception {
+		postmapper.delete(k);
+	}
+
+	@Override
+	public Post get(Integer k) throws Exception {
+		return postmapper.select(k);
+	}
+
+	@Override
+	public List<Post> getall() throws Exception {
+		return postmapper.selectall();
+	}
+	
+	//Paging
+	public List<Post> getListByPaging(Criteria cri) throws Exception{
+		return postmapper.getListByPaging(cri);
+	};
+	
+	//COUNT
+	public int getTotal(Criteria cri) throws Exception{
+		return postmapper.getTotal(cri);
+	}
+	
+}
