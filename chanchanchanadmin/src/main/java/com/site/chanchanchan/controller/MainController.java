3225860118db35f4c08b484f8e5b84bc52e0f8b9
Kyoung Tae <113958094+Park-KyoungTae@users.noreply.github.com>
2023-02-04 02:09:13 +0900

    02/03 박경태 관리자페이지 메인화면 구현

--- chanchanchanadmin/src/main/java/com/site/chanchanchan/controller/MainController.java
+++ chanchanchanadmin/src/main/java/com/site/chanchanchan/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.site.chanchanchan.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.site.chanchanchan.dto.Admin;
+import com.site.chanchanchan.dto.OrderDetail;
+import com.site.chanchanchan.dto.Product;
 import com.site.chanchanchan.service.AdminService;
+import com.site.chanchanchan.service.OrderDetailService;
+import com.site.chanchanchan.service.ProductService;
 
 @Controller
 public class MainController {
@@ -16,9 +22,45 @@ public class MainController {
 	@Autowired
 	AdminService admservice;
 	
+	@Autowired
+	OrderDetailService odservice;
+	
+	@Autowired
+	ProductService productservice;
+	
 	@RequestMapping("/main")
 	public String main(Model model) {
-		return "redirect:/product/info";
+		List<Product> products=null;
+		try {
+			products=productservice.getall();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<OrderDetail> ods= null;
+		int limit=5;
+		try {
+			ods= odservice.bestProduct(new OrderDetail(limit));
+			int rank=1;
+		for(OrderDetail od :ods) {
+			int product_id=0;
+			product_id=od.getProduct_id();
+			String name=productservice.getName(product_id);
+			od.setProduct_name(name);
+			od.setRank(rank++);
+		}
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("product",products);
+		model.addAttribute("bestproduct",ods);
+		
+		model.addAttribute("center","chart/chart1");
+		model.addAttribute("center2","chart/bestproduct");
+		return "main";
 	}
 	
 	@RequestMapping("/login")
