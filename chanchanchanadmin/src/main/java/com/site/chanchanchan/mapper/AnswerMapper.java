af5407db682a299486c7909bd17720455f84bd5e
Kyoung Tae <113958094+Park-KyoungTae@users.noreply.github.com>
2023-02-07 17:46:56 +0900

    02/07 1대1 문의 기능 추가

--- chanchanchanadmin/src/main/java/com/site/chanchanchan/mapper/AnswerMapper.java
+++ chanchanchanadmin/src/main/java/com/site/chanchanchan/mapper/AnswerMapper.java
@@ -14,4 +14,6 @@ import com.site.chanchanchan.frame.MyMapper;
 public interface AnswerMapper extends MyMapper<Integer, Answer>{
 	public List<Answer> getListByPaging(Criteria cri) throws Exception;
 	public int getTotal(Criteria cri) throws Exception;
+	public int answer_existence(Integer id) throws Exception;
+	public Answer select_post_id(Integer post_id) throws Exception;
 }
